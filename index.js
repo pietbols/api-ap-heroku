08dc3f77038c00e9938d4cd272f925b82291dc2c
Piet <79148675+pietbols@users.noreply.github.com>
2022-02-22 19:19:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://cocky-snyder-5a02cf.netlify.app'
+        origin: 'https://websec.herokuapp.com'
     })
 );
 app.options('*', cors());
