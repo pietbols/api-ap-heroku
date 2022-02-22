ceda8016a925ec196226d7fb0a3708c2f978b184
Piet <79148675+pietbols@users.noreply.github.com>
2022-02-22 19:20:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://websec.herokuapp.com'
+        origin: 'https://api-websec.herokuapp.com'
     })
 );
 app.options('*', cors());
